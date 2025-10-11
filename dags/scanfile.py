557327a0a2319d4fb89168229498e3541e0a75d6
kzakr <kzakrzewski17@gmail.com>
2025-10-11 16:49:24 +0200

    dddddddd

--- dags/scanfile.py
+++ dags/scanfile.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperatorm BranchPythonOperator
+from airflow.operators.python import PythonOperatorm, BranchPythonOperator
 #from airflow.operators.branch_operator import BaseBranchOperator
 from airflow.operators.bash import BashOperator
 import datetime
