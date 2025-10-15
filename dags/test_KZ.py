390d21dc8fb13d6a00042c4067f08fa8b1422464
kzakr <kzakrzewski17@gmail.com>
2025-10-15 23:05:57 +0200

    testy

--- dags/test_KZ.py
+++ dags/test_KZ.py
@@ -17,14 +17,16 @@ def _webdriver_options():
 
 with DAG(dag_id = "scanfile", start_date=datetime.datetime(2021, 1, 1), schedule="@daily", catchup = False) as dag:
 
-    get_webdriver_options_b = PythonOperator(
+    get_webdriver_options_bc = PythonOperator(
 
-        task_id = "get_webdriver_options_b",
+        task_id = "get_webdriver_options_bc",
         python_callable = _webdriver_options
     )
 
-    get_webdriver_options_b = PythonOperator(
+    get_webdriver_options_db = PythonOperator(
 
-        task_id = "get_webdriver_options_b",
+        task_id = "get_webdriver_options_db",
         python_callable = _webdriver_options
     )
+
+    get_webdriver_options_bc >> get_webdriver_options_db
