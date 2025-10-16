4d2d7566a8d90774e326a060e0ae61bd1406d8d4
kzakr <kzakrzewski17@gmail.com>
2025-10-16 20:04:28 +0200

    ddd

--- dags/py_files/get.py
+++ dags/py_files/get.py
@@ -2,7 +2,7 @@
 
 
 from selenium import webdriver
-import chromedriver_autoinstaller
+
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 #import time
