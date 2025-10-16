406b916cc28a04699007c2740bedb8cbffd247b1
kzakr <kzakrzewski17@gmail.com>
2025-10-16 22:44:06 +0200

    dddd

--- dags/py_files/get.py
+++ dags/py_files/get.py
@@ -121,64 +121,63 @@ class StockResults:
 
         i = 0
         while i< how_many:
-            for i in grid:
-                        #print(i)
-                d0f_dict = {}
-                #driver.get(url+str(i))
-                
-                time.sleep(1)
-                remote_webdriver = 'chrome'
-                with webdriver.Remote(f'http://{remote_webdriver}:4444/wd/hub', options=options) as launched_driver:
+            
+            remote_webdriver = 'chrome'
+            with webdriver.Remote(f'http://{remote_webdriver}:4444/wd/hub', options=options) as launched_driver:
                     print("kolejna kupa")
 
- 
+                    for i in grid:
+                        #print(i)
+                        d0f_dict = {}
+                        #driver.get(url+str(i))
 
+                        time.sleep(1)
+                        launched_driver.get()
+                        inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
 
-                    inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
-
-                    the_text = inputElement.text
-                    keysy =the_text.split("\n")[0].split(" ")
-                    values  =the_text.split("\n")[1:]
-                        #print(the_text)
-                    values_loop = 0
-                        #print(values_loop)
-                        #print(values)
-                    #print('ww' +str(55))
-                    for value in values:
-
-                        if values_loop == len(keysy):
-                            values_loop = 0
-                            main_df= pd.concat([main_df, temp_df])    
-                            #else:
-                        elif values_loop ==7:
-                            values_loop +=1
-                           # print(value)
-                            #print(keysy)
+                        the_text = inputElement.text
+                        keysy =the_text.split("\n")[0].split(" ")
+                        values  =the_text.split("\n")[1:]
+                            #print(the_text)
+                        values_loop = 0
                             #print(values_loop)
-                            #print(keysy[values_loop])
-                            #print(value)
-                        df_dict[keysy[values_loop]] = value
-                        values_loop +=1
+                            #print(values)
+                        #print('ww' +str(55))
+                        for value in values:
+
+                            if values_loop == len(keysy):
+                                values_loop = 0
+                                main_df= pd.concat([main_df, temp_df])    
+                                #else:
+                            elif values_loop ==7:
+                                values_loop +=1
+                               # print(value)
+                                #print(keysy)
+                                #print(values_loop)
+                                #print(keysy[values_loop])
+                                #print(value)
+                            df_dict[keysy[values_loop]] = value
+                            values_loop +=1
 
 
 
 
 
-                            #df_dict['current_price'] = the_text_2
+                                #df_dict['current_price'] = the_text_2
 
-                        temp_df = pd.DataFrame(df_dict, index=[0])
-                        temp_df['ticker'] = i;
-                        temp_df['time'] = dt_string;
-                        #print(temp_df)
+                            temp_df = pd.DataFrame(df_dict, index=[0])
+                            temp_df['ticker'] = i;
+                            temp_df['time'] = dt_string;
                             #print(temp_df)
+                                #print(temp_df)
 
 
 
-                    del temp_df
-                            #print(main_df)
+                        del temp_df
+                                #print(main_df)
                     
                     
-            self.save_excel_file(file_to_save = main_df, path= "./output_files", file_name= "finviz_" + str(as_of_date[0:8]), extention= '.csv')
+                self.save_excel_file(file_to_save = main_df, path= "./output_files", file_name= "finviz_" + str(as_of_date[0:8]), extention= '.csv')
 
             
             print(how_many)
