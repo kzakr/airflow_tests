000e65f0d9d7038de70988d54ef7260e93c051c3
kzakr <kzakrzewski17@gmail.com>
2025-10-16 22:44:43 +0200

    gseger

--- dags/py_files/get.py
+++ dags/py_files/get.py
@@ -130,11 +130,13 @@ class StockResults:
                         #print(i)
                         d0f_dict = {}
                         #driver.get(url+str(i))
+                        print("zrzut")
 
                         time.sleep(1)
                         launched_driver.get()
                         inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
 
+                        print("zrzut wykonany")
                         the_text = inputElement.text
                         keysy =the_text.split("\n")[0].split(" ")
                         values  =the_text.split("\n")[1:]
