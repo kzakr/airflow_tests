82a54b50aec210575a96fc606339130ed6e9aa52
kzakr <kzakrzewski17@gmail.com>
2025-10-18 22:47:11 +0200

    aaaaddddda

--- dags/py_files/get.py
+++ dags/py_files/get.py
@@ -123,70 +123,60 @@ class StockResults:
         remote_webdriver = 'chrome'
         i = 0
         while i< how_many:
-            for i in grid:
+
+            with webdriver.Remote(f'http://{remote_webdriver}:4444/wd/hub', options=options) as launched_driver:
+
+                for i in grid:
             
 
-                    with webdriver.Remote(f'http://{remote_webdriver}:4444/wd/hub', options=options) as launched_driver:
-                        print("kolejna kupa")
-                        #print(i)
-                        d0f_dict = {}
-                        #driver.get(url+str(i))
-                        print("zrzut")
-                        launched_driver.get(url+str(i))
-
-                        time.sleep(1)
-                        
-                        inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
-
-                        #print(inputElement.text)
-                        the_text = inputElement.text
-                        keysy =the_text.split("\n")[0].split(" ")
-                        values  =the_text.split("\n")[1:]
-                            #print(the_text)
-                        values_loop = 0
-                            #print(values_loop)
-                            #print(values)
-                        #print('ww' +str(55))
-                        for value in values:
-                            print(value)
-
-                            if values_loop == len(keysy):
-                                values_loop = 0
-                                main_df= pd.concat([main_df, temp_df])    
-                                #else:
-                            elif values_loop ==7:
-                                values_loop +=1
-                               # print(value)
-                                #print(keysy)
-                                #print(values_loop)
-                                #print(keysy[values_loop])
-                                print(value)
-                            df_dict[keysy[values_loop]] = value
+                    print("kolejna kupa")
+                    #print(i)
+                    d0f_dict = {}
+                    #driver.get(url+str(i))
+                    print("zrzut")
+                    launched_driver.get(url+str(i))
+                    time.sleep(4)
+                    
+                    inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
+                    #print(inputElement.text)
+                    the_text = inputElement.text
+                    keysy =the_text.split("\n")[0].split(" ")
+                    values  =the_text.split("\n")[1:]
+                        #print(the_text)
+                    values_loop = 0
+                        #print(values_loop)
+                        #print(values)
+                    #print('ww' +str(55))
+                    for value in values:
+                        print(value)
+                        if values_loop == len(keysy):
+                            values_loop = 0
+                            main_df= pd.concat([main_df, temp_df])    
+                            #else:
+                        elif values_loop ==7:
                             values_loop +=1
-
-
-
-
-
-                                #df_dict['current_price'] = the_text_2
-
-                            temp_df = pd.DataFrame(df_dict, index=[0])
-                            temp_df['ticker'] = i
-                            temp_df['time'] = dt_string
+                           # print(value)
+                            #print(keysy)
+                            #print(values_loop)
+                            #print(keysy[values_loop])
+                            print(value)
+                        df_dict[keysy[values_loop]] = value
+                        values_loop +=1
+                            #df_dict['current_price'] = the_text_2
+                        temp_df = pd.DataFrame(df_dict, index=[0])
+                        temp_df['ticker'] = i
+                        temp_df['time'] = dt_string
+                        #print(temp_df)
                             #print(temp_df)
-                                #print(temp_df)
-
-
-
-                        del temp_df
+                    del temp_df
                                 #print(main_df)
                     
                     
-            self.save_excel_file(file_to_save = main_df, path= "/opt/airflow/dags/output_files", file_name= "finviz_" + str(dt_string[0:8]), extention= '.csv')
+                self.save_excel_file(file_to_save = main_df, path= "/opt/airflow/dags/output_files", file_name= "finviz_" + str(dt_string[0:8]), extention= '.csv')
 
             
-            print(how_many)
-            how_many = how_many+1
+                print(how_many)
+                how_many = how_many+1
 
 
         
