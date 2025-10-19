a9a7abd2fa107163d415cf4e1277b2d7361b3cfd
kzakr <kzakrzewski17@gmail.com>
2025-10-19 18:39:38 +0200

    fdaf

--- dags/py_files/get.py
+++ dags/py_files/get.py
@@ -139,17 +139,13 @@ class StockResults:
 
                         launched_driver.get(url+str(i))
                         print(url+str(i))
-                        time.sleep(4)
+                        time.sleep(5)
                         inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
                     #
                     except Exception as ex:
                         print(ex)
-                        time.sleep(8)
-                        launched_driver.get(url+str(i))
-                        print(url+str(i))
-                        time.sleep(5)
-                        inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
-                   
+                        inputElement = ""
+                        
                         
                     #print("zrzut")
                     #try:
@@ -181,43 +177,43 @@ class StockResults:
                     
                     
                     
-                    #the_text = inputElement.text
-                    ##print("zrzut4")
-                    #keysy =the_text.split("\n")[0].split(" ")
-                    ##print("zrzut5")
-                    #values  =the_text.split("\n")[1:]
-                    ##print("zrzut6")
-                    #    #print(the_text)
-                    #values_loop = 0
-                    ##print("zrzut8")
-                    #    #print(values_loop)
-                    #    #print(values)
-                    ##print('ww' +str(55))
-                    #for value in values:
-                    #    #print("zrzut8")
-                    #    
-                    #    if values_loop == len(keysy):
-                    #        values_loop = 0
-                    #        main_df= pd.concat([main_df, temp_df])    
-                    #        #else:
-                    #        #print("###")
-                    #        #print(values_loop)
-                    #    elif values_loop ==7:
-                    #        values_loop +=1
-#
-#
-                    #    #print("zrzut9")
-                    #    df_dict[keysy[values_loop]] = value
-                    #    values_loop +=1
-                    #    #print("zrzut9")
-                    #        #df_dict['current_price'] = the_text_2
-                    #    temp_df = pd.DataFrame(df_dict, index=[0])
-                    #    temp_df['ticker'] = i
-                    #    temp_df['time'] = dt_string
-                    #    #print(temp_df)
-                    #        #print(temp_df)
-                    #del temp_df
-                    #            #print(main_df)
+                    the_text = inputElement.text
+                    #print("zrzut4")
+                    keysy =the_text.split("\n")[0].split(" ")
+                    #print("zrzut5")
+                    values  =the_text.split("\n")[1:]
+                    #print("zrzut6")
+                        #print(the_text)
+                    values_loop = 0
+                    #print("zrzut8")
+                        #print(values_loop)
+                        #print(values)
+                    #print('ww' +str(55))
+                    for value in values:
+                        #print("zrzut8")
+                        
+                        if values_loop == len(keysy):
+                            values_loop = 0
+                            main_df= pd.concat([main_df, temp_df])    
+                            #else:
+                            #print("###")
+                            #print(values_loop)
+                        elif values_loop ==7:
+                            values_loop +=1
+
+
+                        #print("zrzut9")
+                        df_dict[keysy[values_loop]] = value
+                        values_loop +=1
+                        #print("zrzut9")
+                            #df_dict['current_price'] = the_text_2
+                        temp_df = pd.DataFrame(df_dict, index=[0])
+                        temp_df['ticker'] = i
+                        temp_df['time'] = dt_string
+                        #print(temp_df)
+                            #print(temp_df)
+                    del temp_df
+                                #print(main_df)
                     
                     
                 #self.save_excel_file(file_to_save = main_df, path= "/opt/airflow/dags/output_files", file_name= "finviz_" + str(dt_string[0:8]), extention= '.csv')
