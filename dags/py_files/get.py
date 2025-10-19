0cdda0970b5e385a69c216b9387899e0b68bb5c3
kzakr <kzakrzewski17@gmail.com>
2025-10-19 17:20:58 +0200

    ddddddd

--- dags/py_files/get.py
+++ dags/py_files/get.py
@@ -129,52 +129,52 @@ class StockResults:
                 for i in grid:
             
 
-                    print("kolejna kupa")
+                    #print("kolejna kupa")
                     #print(i)
                     d0f_dict = {}
                     #driver.get(url+str(i))
-                    print("zrzut")
+                    #print("zrzut")
                     launched_driver.get(url+str(i))
                     print(url+str(i))
-                    print("zrzut1")
+                    #print("zrzut1")
                     time.sleep(2)
-                    print("zrzut2")
+                    #print("zrzut2")
                     inputElement = launched_driver.find_element(By.XPATH, "//table[@class='styled-table-new is-rounded is-tabular-nums w-full screener_table']")
                     #print(inputElement.text)
-                    print("zrzut3")
+                    #print("zrzut3")
                     the_text = inputElement.text
-                    print("zrzut4")
+                    #print("zrzut4")
                     keysy =the_text.split("\n")[0].split(" ")
-                    print("zrzut5")
+                    #print("zrzut5")
                     values  =the_text.split("\n")[1:]
-                    print("zrzut6")
+                    #print("zrzut6")
                         #print(the_text)
                     values_loop = 0
-                    print("zrzut8")
+                    #print("zrzut8")
                         #print(values_loop)
                         #print(values)
                     #print('ww' +str(55))
                     for value in values:
-                        print("zrzut8")
+                        #print("zrzut8")
                         
                         if values_loop == len(keysy):
                             values_loop = 0
                             main_df= pd.concat([main_df, temp_df])    
                             #else:
-                            print("###")
-                            print(values_loop)
+                            #print("###")
+                            #print(values_loop)
                         elif values_loop ==7:
                             values_loop +=1
                            # print(value)
                             #print(keysy)
-                            print("###")
+                            #print("###")
                             print(values_loop)
                             #print(keysy[values_loop])
                             #print(value)
-                        print("zrzut9")
+                        #print("zrzut9")
                         df_dict[keysy[values_loop]] = value
                         values_loop +=1
-                        print("zrzut9")
+                        #print("zrzut9")
                             #df_dict['current_price'] = the_text_2
                         temp_df = pd.DataFrame(df_dict, index=[0])
                         temp_df['ticker'] = i
