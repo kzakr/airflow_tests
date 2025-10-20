fc7c649d95d8fe14a6f1a834105a4327d7e414fc
kzakr <kzakrzewski17@gmail.com>
2025-10-20 23:14:52 +0200

    svdg

--- dags/chromedriver_with_postgres.py
+++ dags/chromedriver_with_postgres.py
@@ -56,7 +56,6 @@ with DAG(dag_id = "chromedriver_with_postgres", start_date=datetime.datetime(202
     
     create_db_table_task = PostgresOperator(
         task_id='create_db_tables',
-        postgres_conn_id="airflow",
         sql='finwiz_result_create_table.sql'
     )
 
