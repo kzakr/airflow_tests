95c02065544a2ab3ab07e6dad903bd655174aa3d
kzakr <kzakrzewski17@gmail.com>
2025-10-21 21:49:47 +0200

    fdfs

--- dags/finwiz_result_create_table.sql
+++ dags/finwiz_result_create_table.sql
@@ -7,12 +7,12 @@ CREATE TABLE IF NOT EXISTS finwiz_result (
     Company	VARCHAR(100),
     Sector	VARCHAR(100),
     Industry	VARCHAR(100),
-    Country	Market	VARCHAR(100),
+    Country_Market	VARCHAR(100),
     P_E	DECIMAL(38,8),
     Price DECIMAL(38,8)	,
     Change_	DECIMAL(38,8),
     Volume	DECIMAL(38,8),
     ticker	VARCHAR(100),
-    time VARCHAR(100)
+    time_day VARCHAR(100)
 
 );
