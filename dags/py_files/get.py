7cd2a8156728856e092f74ad07984b642e575e8b
kzakr <kzakrzewski17@gmail.com>
2025-10-21 22:40:02 +0200

    dfsd

--- dags/py_files/get.py
+++ dags/py_files/get.py
@@ -182,49 +182,51 @@ class StockResults:
                     
                     
                     
-                    #the_text = inputElement.text
-                    ##print("zrzut4")
-                    #launched_driver.close()
-                    #keysy =the_text.split("\n")[0].split(" ")
-                    ##print("zrzut5")
-                    #values  =the_text.split("\n")[1:]
-                    ##print("zrzut6")
-                    #    #print(the_text)
-                    #values_loop = 0
-                    ##print("zrzut8")
-                    #    #print(values_loop)
-                    #    #print(values)
-                    ##print('ww' +str(55))
-                    #for value in values:
-                    #    #print("zrzut8")
-                    #    
-                    #    if values_loop == len(keysy):
-                    #        values_loop = 0
-                    #        main_df= pd.concat([main_df, temp_df])    
-                    #        #else:
-                    #        #print("###")
-                    #        #print(values_loop)
-                    #    elif values_loop ==7:
-                    #        values_loop +=1
-#
-#
-                    #    #print("zrzut9")
-                    #    try:
-                    #        df_dict[keysy[values_loop]] = value
-                    #        temp_df = pd.DataFrame(df_dict, index=[0])
-                    #        temp_df['ticker'] = i
-                    #        temp_df['time'] = dt_string
-                    #        #print(temp_df)
-#
-                    #    except Exception as ex:
-                    #        print(ex)
-                    #        
-#
-#
-                    #    values_loop +=1
-                    #    #print("zrzut9")
-                    #        #df_dict['current_price'] = the_text_2
-                    #    
+                    the_text = inputElement.text
+                    #print("zrzut4")
+                    launched_driver.close()
+                    keysy =the_text.split("\n")[0].split(" ")
+                    #print("zrzut5")
+                    values  =the_text.split("\n")[1:]
+                    #print("zrzut6")
+                        #print(the_text)
+                    values_loop = 0
+                    #print("zrzut8")
+                        #print(values_loop)
+                        #print(values)
+                    #print('ww' +str(55))
+                    for value in values:
+                        #print("zrzut8")
+                        
+                        if values_loop == len(keysy):
+                            values_loop = 0
+                            main_df= pd.concat([main_df, temp_df])    
+                            #else:
+                            #print("###")
+                            #print(values_loop)
+                        elif values_loop ==7:
+                            values_loop +=1
+
+                        print(value)
+
+
+                        #print("zrzut9")
+                        try:
+                            df_dict[keysy[values_loop]] = value
+                            temp_df = pd.DataFrame(df_dict, index=[0])
+                            temp_df['ticker'] = i
+                            temp_df['time'] = dt_string
+                            #print(temp_df)
+
+                        except Exception as ex:
+                            print(ex)
+                            
+
+
+                        values_loop +=1
+                        #print("zrzut9")
+                            #df_dict['current_price'] = the_text_2
+                        
                             #print(temp_df)
                 #del temp_df
                                 #print(main_df)
