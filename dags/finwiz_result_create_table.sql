faf48936a4be0dc9fbf57892777c78ec09c62781
kzakr <kzakrzewski17@gmail.com>
2025-10-21 21:48:55 +0200

    faegff

--- dags/finwiz_result_create_table.sql
+++ dags/finwiz_result_create_table.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS finwiz_result (
     team_id INT PRIMARY KEY,
     team_name VARCHAR(100),
-    short_name VARCHAR(100)
-    No_	INT
-    Ticker	VARCHAR(100)
-    Company	VARCHAR(100)
-    Sector	VARCHAR(100)
-    Industry	VARCHAR(100)
-    Country	Market	VARCHAR(100)
-    P_E	DECIMAL(38,8)
-    Price DECIMAL(38,8)	
-    Change_	DECIMAL(38,8)
-    Volume	DECIMAL(38,8)
-    ticker	VARCHAR(100)
-    timeVARCHAR(100)
+    short_name VARCHAR(100),
+    No_	INT,
+    Ticker	VARCHAR(100),
+    Company	VARCHAR(100),
+    Sector	VARCHAR(100),
+    Industry	VARCHAR(100),
+    Country	Market	VARCHAR(100),
+    P_E	DECIMAL(38,8),
+    Price DECIMAL(38,8)	,
+    Change_	DECIMAL(38,8),
+    Volume	DECIMAL(38,8),
+    ticker	VARCHAR(100),
+    time VARCHAR(100)
 
 );
