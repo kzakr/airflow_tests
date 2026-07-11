56cec50906a957665a70da088eafda3fece4be7b
kzakr <kzakrzewski17@gmail.com>
2026-07-11 22:50:04 +0200

    dd

--- dags/sql_scipts/result_view_yahoo.sql
+++ dags/sql_scipts/result_view_yahoo.sql
@@ -8,13 +8,13 @@ WITH tickers_increasing as (
 select current_date_, ticker, _date, avg_ticker, avg_date, days_back, avg_days_back from tickers_decreasing_yahoo
 )
 , ranked_data AS (
-  SELECT *, row_number() OVER (PARTITION BY ticker ORDER BY current_date_ DESC, _date DESC) AS rank
+  SELECT *, CASE WHEN increasing_ind IS NOT NULL THEN 'WAS INCREASING' ELSE NULL END AS is_increasing_stauts, row_number() OVER (PARTITION BY ticker ORDER BY current_date_ DESC, _date DESC) AS rank
   FROM (
     select-- tickers_increasing.ticker, tickers_increasing.current_date_ , tickers_increasing._date,
-     tickers_decreasing.ticker, tickers_decreasing._date, tickers_decreasing.days_back, tickers_decreasing.current_date_
+     tickers_decreasing.ticker, tickers_decreasing._date, tickers_decreasing.days_back, tickers_decreasing.current_date_, tickers_increasing.ticker as increasing_ind
 
-  from  tickers_decreasing
---ON tickers_increasing.ticker=tickers_decreasing.ticker-- AND
+  from  tickers_decreasing LEFT JOIN tickers_increasing
+ON tickers_increasing.ticker=tickers_decreasing.ticker-- AND
 --(
 --  tickers_increasing._date = tickers_decreasing.days_back 
 --  OR tickers_increasing.days_back = tickers_decreasing._date
